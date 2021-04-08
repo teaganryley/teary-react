01e13f6e471d85266a5840a1af809b4082ecf1d8
teaganryley <teaganschultz@gmail.com>
2021-04-08 17:49:51 -0400

    flexbox for header

--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -1,14 +1,37 @@
 import styled from 'styled-components';
 
 const StyledHeader = styled.header`
+  display: flex;
+  flex-flow: row;
+  justify-content: flex-start;
+  height: 80px;
+  background: purple;
+  color: #000;
   figure {
+    margin: auto;
     text-align: left;
   }
 `;
 
 const Menu = styled('nav')`
+  margin: auto;
   ul {
+    display: flex;
+    flex-flow: row;
+    justify-content: space-evenly;
+    
     list-style: none;
+    
+    padding: 0;
+    background: deepskyblue;
+    a {
+
+      text-align: center;
+      text-decoration: none;
+      display: block;
+      padding: 1em;
+      color: white;
+    }
   }
 `;
 
